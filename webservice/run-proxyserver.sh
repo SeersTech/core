95edd5d8fa139c61bae59f1df3c49386c53b1302
Siva Palakurthi <sivapalakurthi@seers.in>
2013-12-05 19:34:25 +0530

    restructured Code

--- webservice/run-proxyserver.sh
+++ webservice/run-proxyserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-: ${GIT_CLONE_PATH:?"Need to set non-empty GIT_CLONE_PATH"}
-: ${DEPLOYMENT_LOG_PATH:?"Need to set non-empty DEPLOYMENT_LOG_PATH"}
+: ${GIT_CLONE_PATH=/home/ubuntu/chrome}
+: ${DEPLOYMENT_LOG_PATH=/home/ubuntu/logs}
 forever start -a -l $DEPLOYMENT_LOG_PATH/proxy/forever-proxy.log -o $DEPLOYMENT_LOG_PATH/proxy/out-proxy.log -e $DEPLOYMENT_LOG_PATH/proxy/err-proxy.log $GIT_CLONE_PATH/proxy/index.js
